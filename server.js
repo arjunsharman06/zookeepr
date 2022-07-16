f4d0e060f2cf9516496eaba560b7d5e321d7d49e
ARJUN <arjunsharman06@gmail.com>
2022-07-16 01:11:56 -0400

    Added get functionality

--- server.js
+++ server.js
@@ -63,4 +63,18 @@ app.get('/api/animals',(req,res) => {
     res.json(results);
 });
 
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
 app.listen(PORT,()=> console.log(`Listening on port=${PORT}`));
